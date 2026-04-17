8b521cabf57f5e20c3960b5b4206593fee7ee67c
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2026-04-17 16:52:41 -0400

    Fresh EPG Update: Wed Apr 22 05:03:28 UTC 2026

--- update_epg.py
+++ update_epg.py
@@ -31,6 +31,7 @@ URLS = [
     'https://epgshare01.online/epgshare01/epg_ripper_IT1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_SPORTS1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_FANDUEL1.xml.gz',
+    'https://tvpass.org/epg.xml',
     'https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/nzau/epg.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
     'https://raw.githubusercontent.com/BuddyChewChew/localnow-playlist-generator/refs/heads/main/epg.xml',
