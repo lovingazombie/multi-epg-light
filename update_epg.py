c0e57688f87952ca018840f3d1bd7c4208f90217
lovingazombie <loving.a.zombie@gmail.com>
2026-04-25 12:47:18 -0500

    Fresh EPG Update: Sat Apr 25 17:53:59 UTC 2026

--- update_epg.py
+++ update_epg.py
@@ -32,6 +32,7 @@ URLS = [
     'https://epgshare01.online/epgshare01/epg_ripper_US_SPORTS1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_FANDUEL1.xml.gz',
     'https://tvpass.org/epg.xml',
+    'http://seeapenny.xyz/xmltv.php?username=tmo247line&password=65s4d64vgfdfbae4&type=m3u_plus', 
     'https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/refs/heads/main/playlists/tubi_epg.xml', 
     'https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/nzau/epg.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
